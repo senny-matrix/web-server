e88363645a83d928d78e9591284dd5f39a04fdd8
Rogers Aaron <arumeru@gmail.com>
2016-01-24 14:01:14 +0300

    Updated language - added ! in About Us message

--- server.js
+++ server.js
@@ -6,24 +6,15 @@ var PORT = 3000;
 //    res.send('Hello Express');
 //});
 
-var middleware = {
-    requireAuthentication: function(req, res, next){
-        console.log('Private route hit!');
-        next();
-    },
-    logger: function(req, res, next){
-        console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
-        next();
-    }
-};
+var middleware = require('./middleware.js');
 
 // Global middleware application
 //app.use(middleware.requireAuthentication);
 app.use(middleware.logger);
 // Route Level middleware application
 // About Page route
-app.get('/about!', middleware.requireAuthentication,function(req, res){
-   res.send('About Us'); 
+app.get('/about', middleware.requireAuthentication,function(req, res){
+   res.send('About Us!'); 
 });
 
 app.use(express.static(__dirname + '/public'));
